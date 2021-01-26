0d56359924ebddff1607dd933385091f6d596e21
NickMail <Mizzir@bk.ru>
2021-01-26 14:37:43 +0500

    Добавление контекста данных

---
+++ Elkin/Models/AppCtx.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Elkin.Models
+{
+    public class AppCtx: DbContext
+    {
+        public AppCtx() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Buyer> Courses { get; set; }
+    }
+}
--- Elkin/Models/Buyer.cs
+++ Elkin/Models/Buyer.cs
@@ -1,7 +1,10 @@
-﻿namespace Elkin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elkin.Models
 {
     public class Buyer
     {
+        [Key]
         public int ID_buyer { get; set; }
         public string Last_name { get; set; }
         public string First_name { get; set; }
