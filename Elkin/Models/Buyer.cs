0784e834842bda06df4a0c626b5e52132efdb239
NickMail <Mizzir@bk.ru>
2021-01-26 14:20:03 +0500

    Добавления класса модели

---
+++ Elkin/Models/Buyer.cs
@@ -0,0 +1,9 @@
+﻿namespace Elkin.Models
+{
+    public class Buyer
+    {
+        public int ID_buyer { get; set; }
+        public string Last_name { get; set; }
+        public string First_name { get; set; }
+    }
+}
